efc6f32922b9d230a1439b6d2431ebeea632d974
manantrimbakey <manan.trimbakey@gmail.com>
2024-11-17 14:27:53 +0530

    .

--- src/client/vite.config.ts
+++ src/client/vite.config.ts
@@ -3,10 +3,11 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react()],
   build: {
     outDir: "../../dist/client",
-
+    sourcemap: true,
     rollupOptions: {
       output: {
         entryFileNames: `assets/[name].js`,
--- src/extension.ts
+++ src/extension.ts
@@ -1,22 +1,74 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as path from 'path';
+
+function getWebviewContent(webview: vscode.Webview, extensionUri: vscode.Uri) {
+	// Get path to dist folder
+	const distPathOnDisk = vscode.Uri.joinPath(extensionUri, 'dist', 'client');
+	
+	// Get path to index.html file from dist folder
+	const htmlPathOnDisk = vscode.Uri.joinPath(distPathOnDisk, 'index.html');
+	const cssPathOnDisk = vscode.Uri.joinPath(distPathOnDisk, 'assets', 'index.css');
+	
+	// And get the special URI to use with the webview
+	const baseUri = webview.asWebviewUri(distPathOnDisk);
+	
+	return `<!DOCTYPE html>
+		<html lang="en">
+		<head>
+			<meta charset="UTF-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<title>React App</title>
+			<base href="${baseUri}/">
+		</head>
+		<body>
+			<div id="root"></div>
+			<script type="module" src="${baseUri}/assets/index.js"></script>
+			<link rel="stylesheet" href="${baseUri}/assets/index.css">
+		</body>
+		</html>`;
+}
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	console.log('Extension "test-extension" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "test-extension" is now active!');
+	let currentPanel: vscode.WebviewPanel | undefined = undefined;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('test-extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from test-extension!');
+	const disposable = vscode.commands.registerCommand('test-extension.openWebview', () => {
+		// If we already have a panel, show it
+		if (currentPanel) {
+			currentPanel.reveal(vscode.ViewColumn.One);
+			return;
+		}
+
+		// Otherwise, create a new panel
+		currentPanel = vscode.window.createWebviewPanel(
+			'reactWebview',
+			'React Webview',
+			vscode.ViewColumn.One,
+			{
+				enableScripts: true,
+				localResourceRoots: [
+					vscode.Uri.joinPath(context.extensionUri, 'dist', 'client'),
+					vscode.Uri.joinPath(context.extensionUri, 'dist')
+				]
+			}
+		);
+
+		// Set the webview's HTML content
+		currentPanel.webview.html = getWebviewContent(currentPanel.webview, context.extensionUri);
+
+		// Reset when the current panel is closed
+		currentPanel.onDidDispose(
+			() => {
+				currentPanel = undefined;
+			},
+			null,
+			context.subscriptions
+		);
 	});
 
 	context.subscriptions.push(disposable);
